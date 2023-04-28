5cd9dbb3e974c551c5d6c82a2f789b459fff4d9a
Ouali Mohamed <mohamed.ouali2010gmail.com>
2023-04-28 13:44:43 +0100

    handling form select old values

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,25 +15,29 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}" name="title" id="title"
-                placeholder="Enter post title">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}"
+                name="title" id="title" placeholder="Enter post title">
             @error('title')
                 <small class="text-danger">{{ $message }}</small>
             @enderror
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Content</label>
-            <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content" rows="3" placeholder="Enter content">{{ old('content') }}</textarea>
+            <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content" rows="3"
+                placeholder="Enter content">{{ old('content') }}</textarea>
             @error('content')
                 <small class="text-danger">{{ $message }}</small>
             @enderror
         </div>
         <div class="mb-3">
             <label for="user" class="form-label">Author</label>
-            <select class="form-select @error('user_id') is-invalid @enderror" id="user" name="user_id" aria-label="Default select example">
+            <select class="form-select @error('user_id') is-invalid @enderror" id="user" name="user_id"
+                aria-label="Default select example">
                 <option selected>Select the author</option>
                 @foreach ($authors as $author)
-                    <option value="{{ $author->id }}">{{ $author->name }}</option>    
+                    <option value="{{ $author->id }}" @if (old('user_id') == $author->id) selected="selected" @endif>
+                        {{ $author->name }}
+                    </option>
                 @endforeach
             </select>
             @error('user_id')
@@ -42,10 +46,13 @@
         </div>
         <div class="mb-3">
             <label for="category" class="form-label">Category</label>
-            <select class="form-select @error('category_id') is-invalid @enderror" id="category" name="category_id" aria-label="Default select example">
+            <select class="form-select @error('category_id') is-invalid @enderror" id="category" name="category_id"
+                aria-label="Default select example">
                 <option selected>Select a category</option>
                 @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>    
+                    <option value="{{ $category->id }}" @if (old('category_id') == $category->id) selected="selected" @endif>
+                        {{ $category->name }}
+                    </option>
                 @endforeach
             </select>
             @error('category_id')
